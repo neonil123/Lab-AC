4760e26fea05119b0d385a62de1ab89a6d7ddd55
neonil <neonilr1999@gmail.com>
2022-04-25 12:41:29 +0300

    jumja

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
     @GetMapping(path = "{Name}")
     public String getStudents(@PathVariable("Name") String name) {
-        return "Hello " + name;
+        return "Hello  zdarova " + name;
     }
 
 }
